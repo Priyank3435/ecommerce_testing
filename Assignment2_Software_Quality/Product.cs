ca38e136d8e3efdd20c352028c486384fdce4aa0
priyank <priyankpatel3435@gmail.com>
2024-06-07 17:02:04 -0400

    Complete Product Class and Setup NUnit Project

---
+++ Assignment2_Software_Quality/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignment2_Software_Quality
+{
+    namespace Groccery
+    {
+        public class Product
+        {
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+            public int Stock { get; set; }
+
+            public Product(int productId, string productName, decimal price, int stock)
+            {
+                ProductID = productId;
+                ProductName = productName;
+                Price = price;
+                Stock = stock;
+            }
+
+            public void DecreaseStockCount(int stock)
+            {
+                if (stock > 0)
+                {
+                    Stock += stock;
+                }
+            }
+
+            public void DecreaseStockCount(int stock)
+            {
+                if (stock > 0 && Stock >= stock)
+                {
+                    Stock -= stock;
+                }
+            }
+           
+        }
+    }
+
+}
---
+++ Test_Assignment2_Software_Quality/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_Assignment2_Software_Quality
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
